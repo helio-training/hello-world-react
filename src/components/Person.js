b98bfa4eb9e6d6f3d8e3ba79fb52df484d19aee5
Tyler Garlick <tjgarlick@gmail.com>
2017-02-28 11:06:19 -0700

    Additions

--- src/components/Person.js
+++ src/components/Person.js
@@ -5,15 +5,23 @@ class Person extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = { firstName: props.firstName, lastName: props.lastName };
+    console.log(this.state);
   }
 
-  render() {
+  handleClick = (e) => {
+    console.log(e);
+  };
 
-    const { firstName, lastName } = this.props;
+  render() {
+    const { firstName, lastName } = this.state;
 
     return (
       <div className="Person">
-        <h3>{firstName} {lastName}</h3>
+        <h3 title={`${firstName} ${lastName}`} onClick={this.handleClick}>{firstName} {lastName}</h3>
+        <input type="text" placeholder="First Name" value={this.state.firstName} onChange={e => this.setState({ firstName: e.target.value})} />
+        <input type="text" placeholder="Last Name" value={this.state.lastName} onChange={e => this.setState({ lastName: e.target.value})} />
       </div>
     );
   }
